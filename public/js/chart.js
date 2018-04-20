0e6472d9bf30cace91b4e08f30355533d22aa27b
mblaul <mattblaul@gmail.com>
2018-04-19 22:58:30 -0400

    Turns out since the d3.json only takes json files. I just moved data to the .data() method in the canvas.selectAll() statement. Graphs now display on page.

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,26 +1,23 @@
-d3.json(JSON.stringify(freedomlist), function (data) {
-  var width = 500;
-  var height = 500;
-  var heightScale = d3.scaleLinear()
-    .domain([0, 500])
-    .range([0, height]);
+var width = 1500;
+var height = 1500;
+var heightScale = d3.scaleLinear()
+  .domain([0, 1])
+  .range([0, height]);
 
-  var color = d3.scaleLinear()
-    .domain([0,500])
-    .range(["red","blue"]);
+var color = d3.scaleLinear()
+  .domain([0,1])
+  .range(["orange","blue"]);
 
-  var canvas =  d3.select("main")
-    .append("svg")
-    .attr("width", 500)
-    .attr("height", 500);
+var canvas =  d3.select("main")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-  var bars = canvas.selectAll("rect")
-    .data(data)
-    .enter()
-      .append("rect")
-      .attr("width", 45)
-      .attr("height", function(d) { return d; })
-      .attr("fill", function(d){ return color(d); })
-      .attr("x", function(d, i){ return i * 100; });
-  });
- console.log(d3);
+var bars = canvas.selectAll("rect")
+  .data(freedomlist)
+  .enter()
+    .append("rect")
+    .attr("width", 10)
+    .attr("height", function(d) { return d.Freedom * 1000; })
+    .attr("fill", function(d){ return color(d.Freedom); })
+    .attr("x", function(d, i){ return i * 5; });
