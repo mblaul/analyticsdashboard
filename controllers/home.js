7a056a0d8d4459571aa2c64bba2dd50b7eb9d62f
mblaul <mattblaul@gmail.com>
2018-04-22 22:17:21 -0400

    Working on generating bar charts from multiple sequelize queries.

--- controllers/home.js
+++ controllers/home.js
@@ -1,15 +1,24 @@
 var db = require('../models/index.js');
-var d3 = require('d3');
 
 module.exports.index_get = (req, res, next) => {
 
   db.report_ty.findAll({
-    raw: true,
-    limit: 10,
-    attributes: ['Country','Freedom']
-  }).then(freedomlist =>{
-    return res.render('index', {freedomlist:freedomlist});
-  })
+      raw: true,
+      limit: 10,
+      attributes: ['Country','Freedom'],
+      order: [['Freedom', 'DESC']]
+    }).then(topfreedomindex =>{
+      return topfreedomindex;
+    });
 
-  // return res.render('index',  {d3n:d3n});
+  db.report_ty.findAll({
+      raw: true,
+      limit: 10,
+      attributes: ['Country','Freedom'],
+      order: [['Freedom', 'ASC']]
+    }).then(btmfreedomindex =>{
+      return btmfreedomindex;
+    });
+
+  return res.render('index',  {btmfreedomindex:btmfreedomindex, topfreedomindex:topfreedomindex});
 }
